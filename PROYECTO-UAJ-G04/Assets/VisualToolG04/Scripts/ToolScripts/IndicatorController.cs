2d5e2c33a711ca09759e0a22796f0055804adaff
asegar01 <asegar01@ucm.es>
2024-05-18 18:03:50 +0200

    Documentación IndicatorController y Transmitter
    Co-Authored-By: simekons <62706897+simekons@users.noreply.github.com>

--- PROYECTO-UAJ-G04/Assets/VisualToolG04/Scripts/ToolScripts/IndicatorController.cs
+++ PROYECTO-UAJ-G04/Assets/VisualToolG04/Scripts/ToolScripts/IndicatorController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Clase que gestiona el almacenamiento de los indicadores.
+/// </summary>
 public class IndicatorController : MonoBehaviour
 {
     /// <summary>
@@ -10,63 +13,123 @@ public class IndicatorController : MonoBehaviour
     /// </summary>
     public static IndicatorController instance;
     
+    /// <summary>
+    /// Cola de sonidos a procesar.
+    /// </summary>
     private Queue<IndicatorInfo> _sounds = new Queue<IndicatorInfo>();
+
+    /// <summary>
+    /// Diccionario de indicadores activos.
+    /// </summary>
     private Dictionary<UInt64,GameObject> _indicators = new Dictionary<UInt64,GameObject>();
+
+    /// <summary>
+    /// Cola de indicadores a destruir. 
+    /// </summary>
     private Queue<UInt64> _indicatorsToDestroy = new Queue<UInt64>();
+
+    /// <summary>
+    /// Contador del número de indicadores.
+    /// </summary>
     private UInt64 m_id;
+
+    /// <summary>
+    /// Tamaño del círculo en el canvas.
+    /// </summary>
     [SerializeField]
     [Range(1, 100)]
     int circleSize = 50;
+
+    /// <summary>
+    /// Radio del círculo en el canvas.
+    /// </summary>
     private int radius;
+
+    /// <summary>
+    /// Instancia el controlador, el radio del círculo y el contador.
+    /// </summary>
     private void Awake()
     {
-        radius = Mathf.Min(Screen.width, Screen.height) /2;
-        radius=radius*circleSize/100;
+        radius = Mathf.Min(Screen.width, Screen.height) / 2;
+        radius = radius * circleSize / 100;
         if (IndicatorController.instance == null)
         {
             instance = this;
             m_id = 0;
         }
         else
-            Debug.LogError("Hay más de un IndicatorController");
+            Debug.LogError("Hay más de un IndicatorController.");
     }
-    
+
+    /// <summary>
+    /// Mantiene la gestión de los indicadores que se van a destruir y actualiza la lista.
+    /// </summary>
     private void LateUpdate()
     {
         foreach(UInt64 key in _indicatorsToDestroy)
         {
-            GameObject go= _indicators[key];
+            GameObject go = _indicators[key];
             _indicators.Remove(key);
-           Destroy(go);
-            
+            Destroy(go);
         }   
         _indicatorsToDestroy.Clear();
     }
 
+    /// <summary>
+    /// Recibe un evento de sonido con la información del indicador y lo inserta en la cola para su procesamiento.
+    /// </summary>
+    /// <param name="cSound"></param>
     public void ReceiveEvent(IndicatorInfo cSound)
     {
         _sounds.Enqueue(cSound);
     }
+
+    /// <summary>
+    /// Cola de sonidos a procesar.
+    /// </summary>
     public Queue<IndicatorInfo> Sounds { get { return _sounds; } }
+
+    /// <summary>
+    /// Diccionario de indicadores activos.
+    /// </summary>
     public Dictionary<UInt64, GameObject> Indicators { get { return _indicators; } }
 
+    /// <summary>
+    /// Añade un nuevo indicador al diccionario y lo coloca en el canvas.
+    /// </summary>
+    /// <param name="id">Identificador único del sonido.</param>
+    /// <param name="go">Objeto asociado al indicador.</param>
     public void AddIndicator(UInt64 id,GameObject go)
     {
-        _indicators.Add(id,go);
+        _indicators.Add(id, go);
         GameObject current = go;
         Vector3 aux = current.GetComponent<RectTransform>().localPosition;
         current.transform.SetParent(transform);
         current.transform.localPosition = aux;
         current.SetActive(true);
     }
+
+    /// <summary>
+    /// Marca un indicador para ser destruido.
+    /// </summary>
+    /// <param name="id">Identificador único del sonido</param>
     public void RemoveIndicator(UInt64 id)
     {
         _indicatorsToDestroy.Enqueue(id);
     }
+
+    /// <summary>
+    /// Devuelve el ID del indicador que le corresponde al Transmitter que lo ha solicitado.
+    /// </summary>
+    /// <returns>Nuevo identificador único del sonido.</returns>
     public UInt64 AskForID()
     {
         return m_id++;
     }
+
+    /// <summary>
+    /// Radio del círculo en el canvas.
+    /// </summary>
     public int Radius
     {
         get { return radius; }
--- PROYECTO-UAJ-G04/Assets/VisualToolG04/Scripts/ToolScripts/Transmitter.cs
+++ PROYECTO-UAJ-G04/Assets/VisualToolG04/Scripts/ToolScripts/Transmitter.cs
@@ -7,52 +7,110 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class Transmitter : MonoBehaviour
 {
+    /// <summary>
+    /// Imagen asociada con el indicador del sonido.
+    /// </summary>
     [SerializeField]
     UnityEngine.UI.RawImage image;
+
+    /// <summary>
+    /// Distancia máxima a la que se puede escuchar el sonido.
+    /// </summary>
     [SerializeField]
     float listenableDistance = 10.0f;
-    IndicatorInfo sound;
-    IndicatorController soundController;
+
+    /// <summary>
+    /// Instancia que contiene la información del indicador
+    /// </summary>
+    IndicatorInfo indicator;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    IndicatorController indicatorController;
+
+    /// <summary>
+    /// Componente de audio asociado al indicador.
+    /// </summary>
     AudioSource audioSource;
+
+    /// <summary>
+    /// Color del indicador.
+    /// </summary>
     [SerializeField]
     Color shaderColor;
+
+    /// <summary>
+    /// Factor de escala del indicador.
+    /// </summary>
     [SerializeField]
-    float scaleIndicator=1.0f;
+    float scaleIndicator = 1.0f;
+
+    /// <summary>
+    /// Icono asociado al indicador.
+    /// </summary>
     [SerializeField]
     Sprite icon;
+
+    /// <summary>
+    /// Factor de escala del icono asociado al indicador.
+    /// </summary>
     [SerializeField]
     private float scaleIcon = 1.0f;
-    private UInt64 m_id;
-    [SerializeField]
-    float vibration= 15.0f;
-    
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Identificador único para el sonido.
+    /// </summary>
+    private UInt64 m_id;
+
+    /// <summary>
+    /// Nivel de vibración del indicador.
+    /// </summary>
+    [SerializeField]
+    float vibration = 15.0f;
+   
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         m_id = IndicatorController.instance.AskForID();
-        print(gameObject.name+ m_id);
-        sound = new IndicatorInfo(transform.position, image, listenableDistance, shaderColor, scaleIndicator, icon, scaleIcon, m_id, vibration);
-        if ((soundController = IndicatorController.instance) == null)
+
+        // Crea un nuevo indicador con los parámetros iniciales.
+        indicator = new IndicatorInfo(
+            transform.position, 
+            image, 
+            listenableDistance, 
+            shaderColor, 
+            scaleIndicator, 
+            icon, 
+            scaleIcon, 
+            m_id, 
+            vibration
+        );
+
+        if ((indicatorController = IndicatorController.instance) == null)
         {
-            Debug.LogError("No hay CanvasSoundController");
+            Debug.LogError("No hay CanvasSoundController.");
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Si se está reproduciendo algún sonido envía el evento.
         if (audioSource.isPlaying)
         {
             SendEvent();
         }
     }
 
+    /// <summary>
+    /// Envía un evento de sonido al controlador de sonido del canvas.
+    /// </summary>
     void SendEvent()
     {
-        sound.Position = transform.position;
-        sound.Color = shaderColor;
-        soundController.ReceiveEvent(sound);
+        // Actualiza la posición del sonido.
+        indicator.Position = transform.position;
+
+        // Envía el sonido actualizado al controlador.
+        indicatorController.ReceiveEvent(indicator);
     }
 }
